d26db5bac68d08dbe80378a7298fa4c42e846b0b
joontai98 <joontae98@gmail.com>
2020-11-17 14:15:23 +0900

    Fix: Fix SeleniumTest.java

--- src/Selenium/SeleniumTest.java
+++ src/Selenium/SeleniumTest.java
@@ -1,20 +1,18 @@
 package Selenium;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
 
     public static void main(String[] args) {
 
         // 프로젝트의 진행 순서
-        // 1. selenium 라이브러리 다운
-        // 2. 마우스, 키보드 이벤트 실행
-        // 3. 로그인 후 메일 확인
+        // 1. selenium 라이브러리 다운 *크롬버전과 크롬드라이브버전 확인
+        // 2. html 태그의 id 확인후 findElement 메서드를 이용하여 실행
+        // 3. 로그인 후 메일 갯수 가져와서 출력
         SeleniumTest selTest = new SeleniumTest();
         selTest.crawl();
 
@@ -35,14 +33,9 @@ public class SeleniumTest {
 
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 
+        driver = new ChromeDriver();
 
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("ignoreProtectedModeSettings", true);
-        driver = new ChromeDriver(options);
-
-        base_url = "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F";
-
-
+        base_url = "https://www.daum.net/";
 
     }
 
@@ -51,31 +44,29 @@ public class SeleniumTest {
         try {
             driver.get(base_url);
 
-
-            webElement = driver.findElement(By.className("link_loginlink_kakaoid"));
+            webElement = driver.findElement(By.className("link_login"));
+            webElement.click();
             Thread.sleep(1000);
 
-            webElement = driver.findElement(By.id("id_email_2"));
-            String website_id =" ";
+            webElement = driver.findElement(By.id("id"));
+            String website_id = ""; //id 입력
             webElement.sendKeys(website_id);
             Thread.sleep(1000);
 
-            webElement = driver.findElement(By.id("id_password_3"));
-            String website_pw =" ";
+            webElement = driver.findElement(By.id("inputPwd"));
+            String website_pw = ""; //password 입력
             webElement.sendKeys(website_pw);
             Thread.sleep(1000);
 
-
-            webElement = driver.findElement(By.className("btn_g btn_confirm submit"));
+            webElement = driver.findElement(By.id("loginBtn"));
             webElement.submit();
             Thread.sleep(1000);
 
-//            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//
-//            webElement = driver.findElement(By.xpath("//*[@id=\"mArticle\"]/div[1]/div[2]/ul/li[1]/em/a"));
-//            String mailCnt = webElement.getText();
-//
-//            System.out.println("메일의 갯수 : " + mailCnt);
+            //메일 갯수를 가져오기 위한 경로 = xpath
+            webElement = driver.findElement(By.xpath("//*[@id=\"mArticle\"]/div[1]/div[2]/ul/li[1]/em/a"));
+            String mailCnt = webElement.getText();
+
+            System.out.println("메일의 갯수 : " + mailCnt);
 
 
         } catch (Exception e) {
@@ -84,7 +75,7 @@ public class SeleniumTest {
 
         } finally {
 
-            //driver.close();
+            driver.close();
         }
 
     }
